b14046703772d4f75ad3c5aea483c8795f9b6477
Nik Hammer-Ellis <nikhammerellis@gmail.com>
2017-09-17 15:23:06 -0700

    changed to https for weather api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'a456dd5e0f012c9e21125c8a92e492b7';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
